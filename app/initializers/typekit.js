a4341344c0ff45b9cbd7b1abb0d11b01ec8ec738
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-01-27 21:52:41 +1100

    typekit goodness updated adapter to use correct firebase url from config updated environment with typekit id, got rid of underscores added typekit_id to dotenv added typekit initializer added ember-inject-script for typekit initializer basic nav-bar component with stock foundation nav bar on the top

---
+++ app/initializers/typekit.js
@@ -0,0 +1,15 @@
+/* global Typekit */
+import injectScript from 'ember-inject-script';
+import config from '../config/environment';
+
+export function initialize(/* container, application */) {
+  var url = '//use.typekit.net/' + config.typekitId + '.js';
+  injectScript(url).then(function() {
+    Typekit.load();
+  });
+}
+
+export default {
+  name: 'typekit',
+  initialize: initialize
+};
--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -4,5 +4,5 @@ import DS from 'ember-data';
 import config from '../../config/environment';
 
 export default DS.FirebaseAdapter.extend({
-  firebase: new Firebase(`https://${config.firebase_instance}.firebaseIO.com`)
+  firebase: new Firebase(`https://${config.firebaseUrl}.firebaseIO.com`)
 });

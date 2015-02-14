b1f7224406f748c167b5c2dcddc9489c2bb8c2f0
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-02-14 17:33:29 +1100

    google map component with failing test

--- app/pods/components/google-map/component.js
+++ app/pods/components/google-map/component.js
@@ -1,29 +1,57 @@
 import Ember from 'ember';
 
-var {
-  on,
-  computed,
-  get,
-  getWithDefault
-} = Ember;
+var on             = Ember.on;
+var computed       = Ember.computed;
+var get            = Ember.get;
+var set            = Ember.set;
+var observer       = Ember.observer;
 
 export default Ember.Component.extend({
-  classNames: [ 'googleMap' ],
-
-  _getApi: computed('api', function() {
-    return getWithDefault(this, 'api', window.google);
-  }),
+  classNames : [ 'googleMap' ],
+  map        : null,
+  api        : window.google,
 
   _drawMap: on('didInsertElement', function() {
-    var el = this.$();
-    var google = get(this, '_getApi');
+    let lat = get(this, 'lat');
+    let lng = get(this, 'lng');
+
+    var el     = this.$();
+    var google = get(this, 'api');
 
     var options = {
-      center: new google.maps.LatLng(this.get('lat'), this.get('lng')),
-      zoom: 11,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      center    : new google.maps.LatLng(lat, lng),
+      zoom      : 11,
+      mapTypeId : google.maps.MapTypeId.ROADMAP
     };
 
-    new google.maps.Map(el[0], options);
-  })
+    let map = new google.maps.Map(el[0], options);
+
+    set(this, 'map', map);
+  }),
+
+  markers: computed('listings.@each.{lat,lng}', 'api', function() {
+    let { maps: GoogleMapsAPI } = get(this, 'api');
+    let markers = get(this, 'listings').map((listing) => {
+      let lat = listing.get('lat');
+      let lng = listing.get('lng');
+
+      return new GoogleMapsAPI.Marker({
+        listingId : listing.get('id'),
+        position  : new GoogleMapsAPI.LatLng(lat, lng)
+      });
+    });
+
+    return markers;
+  }),
+
+  _mountMarkersToMap: observer('markers.@each', 'map', function() {
+    let markers = get(this, 'markers');
+    let map     = get(this, 'map');
+
+    if (!map || !markers) { return; }
+
+    markers.forEach((marker) => {
+      marker.setMap(map);
+    });
+  }).on('init')
 });

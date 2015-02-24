ec384531c21005b858cc2bca9077154e4320c710
Chris Atkins <christopherlionelatkins@gmail.com>
2015-02-24 23:32:27 +1100

    Added a stubbed listing page

--- app/pods/listing/route.js
+++ app/pods/listing/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.findById('listing', params.listing_id);
+  },
+
+  setupController: function(controller, model) {
+    controller.set('listing', model);
+  }
 });

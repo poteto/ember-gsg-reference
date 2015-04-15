cffe0e8d32ebabc5fc0828a8655fcebc029de12b
Brian Clifton <brian@clifton.me>
2015-04-14 20:43:29 -0700

    Switches over to using pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,8 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-getting-started',
+    podModulePrefix: 'ember-getting-started/pods',
+    usePods: true,
     typekitId: process.env.TYPEKIT_ID,
     environment: environment,
     firebase: 'https://' + process.env.FIREBASE_URL + '.firebaseio.com/',
